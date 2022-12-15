8417848fe19db4c9e1399c466671befe10eccc56
[Kaustav Giri] <[kaustavgiri2017@gmail.com]>
2022-12-16 00:53:00 +0530

    Fully Done

---
+++ Blog/blogSite/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from blogSite import views
+from . import views
+
+urlpatterns = [
+    path('',views.FirstPage,name='firstPage'),
+    path('posts',views.AllPost,name='All-Posts'),
+    path('posts/<slug:slug>',views.PostDetails,name='Post-Details')
+]
---
+++ Blog/blogSite/views.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render
+from datetime import date
+
+# Create your views here.
+
+posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountain.jpeg",
+        "author": "Kaustav",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpeg",
+        "author": "Kaustav",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpeg",
+        "author": "Kaustav",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
+
+def get_date(post):
+    return post['date']
+
+
+def FirstPage(req):
+    filteredPost = sorted(posts, key=get_date, reverse=True)
+    return render(req,'blogSite/index.html',{
+        'posts':filteredPost
+    })
+
+def AllPost(req):
+    return render(req,'blogSite/AllPost.html',{
+        'Posts':posts
+    })
+
+def PostDetails(req,slug):
+    for i in posts:
+        if i['slug'] == slug:
+            return render(req,'blogSite/PostDetail.html',{
+                'postData': i
+            })
+        else:
+            pass
